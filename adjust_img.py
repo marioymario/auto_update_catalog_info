6e50067ac064d68ef1bd72942593df7c44ffb0c6
“gato” <mario.hevia@gmail>
2023-01-29 07:16:34 -0800

    Adjust the script to the requirements

--- adjust_img.py
+++ adjust_img.py
@@ -1,27 +1,13 @@
 import os
 from PIL import Image
 
-def transforma(rotation, new_size, ext, in_loc, out_loc):
+path = '/suppliers-data/images/'
+for file in os.listdir('/suppliers-data/images'):
+    if file.endswith('.tiff'):
+        split_file = file.split('.')
+        name = split[0] + '.jpeg'
+        imagen = Image.open(path + file).convert('RGB')
+        imagen.resize((600, 400))
+        imagen.save(path + name, 'JPEG')
 
-    rotation = rotation
-    new_size = new_size
-    in_loc = in_loc
-    out_loc = out_loc
-    new_ext = ext
-    counter = 0
-
-    for filename in os.listdir(in_loc):
-        if not filename.endswith('.tiff'):
-            continue
-        ##
-        in_path = os.path.join(in_loc, filename)
-
-        # Opening and adjusting the image.
-        im = Image.open(in_path)
-        new_img = im.rotate(rotation).resize(new_size).convert('RGB')
-        # Saving image at proper location
-        out_path = os.path.join(out_loc, filename[:-4] + new_ext)
-        new_img.save(str(out_path))
-        counter +=1
-    print(counter)
 
